a7001852e379f446ff4224f48d8fe7a5af06e3af
Mufo5 <mufogaming@gmail.com>
2024-09-09 21:21:33 +0200

    Ajouts des consts persos script

--- script.js
+++ script.js
@@ -3,11 +3,358 @@ const characters = {
         image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
         video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
     },
+    "Alhaitham": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Aloy": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
     "Amber": {
         image: "https://uploadstatic-sea.mihoyo.com/contentweb/20200928/2020092819462155694.png",
         video: "https://www.youtube.com/embed/S2KeVTg91RE"
-    }
-    // Ajoutez ici d'autres personnages selon vos préférences
+    },
+    "Arataki Itto": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Arlecchino": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Baizhu": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Barbara": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Beidou": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Benett": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Candace": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Charlotte": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Chevreuse": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Chiori": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Chongyun": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Clorinde": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Collei": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Cyno": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Dehya": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Diluc": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Diona": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Dori": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Emilie": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Eula": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Faruzan": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Fischl": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Freminet": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Furina": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Gaming": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Ganyu": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Gorou": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Hu Tao": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Jean": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kachina": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kaedara Kazuha": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kaeya": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kamisato Ayaka": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kamisato Ayato": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kaveh": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Keqing": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kinich": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Aloy": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kirara": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Klee": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kujou Sara": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Kuki Shinobu": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Layla": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Lisa": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Lynette": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Lyney": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Mika": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Mona": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Mualani": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Nahida": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Navia": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Neuvillette": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Nilou": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Ningguang": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Noelle": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Noelle": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Qiqi": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Raiden Shogun": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Razor": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Rosaria": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Sangonomiya Kokomi": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Sayu": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Sethos": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Shenhe": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Shikanoin Heizou": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Sigewinne": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Sucrose": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Tartaglia": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Thomas": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Tighnari": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Traveler": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Venti": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Wriothesley": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Xianling": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Xianyun": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Xiao": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Xilonen": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Xingqiu": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Xinyan": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Yae Miko": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Yaoyao": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Yelan": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Yoimiya": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Yun Jin": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
+    "Zhongli": {
+        image: "https://uploadstatic-sea.mihoyo.com/contentweb/20201204/2020120419111276595.png",
+        video: "https://www.youtube.com/embed/Jy3Y3Rpml3A"
+    },
 };
 
 let selectedCharacters = [];
